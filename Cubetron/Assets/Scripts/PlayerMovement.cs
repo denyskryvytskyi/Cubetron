091b53c9d9d4a232cc1a6bd7d6bea72a42fec600
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2019-10-30 18:53:36 +0200

    some fixes

--- Cubetron/Assets/Scripts/PlayerMovement.cs
+++ Cubetron/Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,14 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
     
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float forwardForce = 1f;
+    public float horizontalForce = 10f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-
-        if (Input.GetKey("d") || Input.GetKey("right"))
-        {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("a") || Input.GetKey("left"))
-        {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
+        rb.AddForce(Input.GetAxisRaw("Horizontal") * horizontalForce * Time.deltaTime, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        
         if(rb.position.y < -1f)
         {
             FindObjectOfType<GameManager>().EndGame();
